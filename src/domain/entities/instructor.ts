71c3ddc7ad9c2878538d7bdab46f2e11155f5204
Kellen Pinho <kellen.neves@vanguardatech.com>
2026-03-13 22:01:21 -0300

    feat: create entity

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,11 +1,15 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { randomUUID } from "node:crypto";
 
-export class Instructor {
-  public id: string;
-  public name: string;
+interface InstructorProps {
+  name: string;
+}
 
-  constructor(name: string, id?: string) {
-    this.name = name;
-    this.id = id ?? randomUUID();
+export class Instructor extends Entity<InstructorProps> {
+  static create(props: InstructorProps, id?: UniqueEntityID) {
+    const instructor = new Instructor(props, id);
+
+    return instructor;
   }
 }
--- src/domain/entities/student.ts
+++ src/domain/entities/student.ts
@@ -1,11 +1,14 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { randomUUID } from "node:crypto";
 
-export class Student {
-  public id: string;
-  public name: string;
+interface StudentProps {
+  name: string;
+}
+export class Student extends Entity<StudentProps> {
+  static create(props: StudentProps, id?: UniqueEntityID) {
+    const answer = new Student(props, id);
 
-  constructor(name: string, id?: string) {
-    this.name = name;
-    this.id = id ?? randomUUID();
+    return answer;
   }
 }
